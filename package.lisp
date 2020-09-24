3c7ab7dca123cba8c85b939d362a284dbab85081
Nick Matvyeyev <mnasoft@gmail.com>
2020-09-24 07:47:14 +0300

    Чт 24 сен 2020 04:47:14 UTC

--- package.lisp
+++ package.lisp
@@ -7,6 +7,48 @@
    *test-labes-list*
    *test-hash-lables*
    )
+  (:export make-plot extract
+         set-*range
+         *point-type-open*
+         help-set-grid
+         *point-type-fill-box*
+         format-n-string
+         <arc>-radius <line>
+         set-*tics
+         stacked-chart-data
+         make-int-range
+         *point-type-box*
+         help-set-*range
+         set-title
+         set-grid
+         <point>-coords
+          <gnuplot-vector-printer>
+         out-func-polynom-fit
+         *color-names*
+         out-plot
+         <arc>-end-angle
+         set-m*tics
+         out
+         help-set-*tics
+         *point-type-all*
+         out-table
+         *point-type-fill*
+         <arc>
+         out-vectors-to-file
+         <circle>-radius
+         help-set-m*tics
+         <arc>-center
+          make-int-list <arc>-start-angle
+          <circle> filter <circle>-center
+          set-polar help-set-key
+          help-set-polar help-set-title
+          help-colorspec <point>
+          make-func-polynom-fit set-key
+          <line>-end <line>-start
+          <gnuplot-vector-printer>-stream set-theta
+          polar *point-type-box-fill*
+          make-hash-table-lables
+          )
   )
 
 ;;;; (declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
